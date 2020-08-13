3500303d7ae4f7e714fa618d7f8322d87089d2d3
Paul Louka <poullouka@pouls-mbp.myfiosgateway.com>
2020-08-12 21:16:45 -0400

    Updates

---
+++ precache-manifest.555c86c0162b8d9855dfe798dc6cb345.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d8b932e39c8ad039ac327242a6c0df",
+    "url": "/Paul-react-portfolio/index.html"
+  },
+  {
+    "revision": "ab88ac1a77c2864cbf7e",
+    "url": "/Paul-react-portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1a841b0ca3069b1af48e",
+    "url": "/Paul-react-portfolio/static/css/main.b28b49e6.chunk.css"
+  },
+  {
+    "revision": "ab88ac1a77c2864cbf7e",
+    "url": "/Paul-react-portfolio/static/js/2.3f72f909.chunk.js"
+  },
+  {
+    "revision": "1a841b0ca3069b1af48e",
+    "url": "/Paul-react-portfolio/static/js/main.cd8272cd.chunk.js"
+  },
+  {
+    "revision": "c419325cb5e7e2236431",
+    "url": "/Paul-react-portfolio/static/js/runtime~main.6e23b39b.js"
+  },
+  {
+    "revision": "dd34442d95a83ca62451084f650efc07",
+    "url": "/Paul-react-portfolio/static/media/Vmarine.dd34442d.jpg"
+  },
+  {
+    "revision": "f36c58e259182a743b0a8dfd1c245550",
+    "url": "/Paul-react-portfolio/static/media/aguaDeLuz.f36c58e2.png"
+  },
+  {
+    "revision": "2739b2c5e6b177c35defb08fcf4d8a6a",
+    "url": "/Paul-react-portfolio/static/media/myImage.2739b2c5.png"
+  },
+  {
+    "revision": "ac82d97d170d742395675707f640e98d",
+    "url": "/Paul-react-portfolio/static/media/todolist.ac82d97d.png"
+  }
+]);
